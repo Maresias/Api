976fa000ebe99ffadd72e52b64ebd4667e8865c6
AlexandreRCS <ale.maresias@gmail.com>
2024-02-21 21:19:41 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ app.get("/users", (request, response) =>{
     response.send(`Page: ${page} View: ${limit}`)
 })
 
+app.post("/user", (request, response) => {
+    response.send('Agora você chamou o POST')
+})
+
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
