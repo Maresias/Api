6229ded81849064a167145ccd3e502ffb6c0f6c1
AlexandreRCS <ale.maresias@gmail.com>
2024-02-25 19:30:02 -0300

    Update UsersController.js

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,13 @@
 class UsersController{
+    /** REGRAS RECOMENDADAS PARA USO DOS CONTROLLES
+     * Index - GET para listar vários registros.
+     * Show - GET para exivir um registro especifico.
+     * create - POST para criar um registro.
+     * Update - PUT para atualizar um registro.
+     * delete - DELETE para remover um registro.
+     */
 
+    
 }
 
 
