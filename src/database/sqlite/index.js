a8a4d68c9dd1e05ff0c88e456d3993115f093ecf
AlexandreRCS <ale.maresias@gmail.com>
2024-02-26 23:02:38 -0300

    Update index.js

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -4,6 +4,13 @@ const path = require("path")
 
 async function sqliteConnection(){
     const database = await sqlite.open({
-        filename: path.resolve(__dirname, "..", "database.db")
+        filename: path.resolve(__dirname, "..", "database.db"),
+        driver: sqlite3.Database
     })
+
+    return database
+
 }
+
+
+module.exports = sqliteConnection
