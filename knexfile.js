dd9a88bda2c0b05ec6b6bf813955cf415b5c88f9
AlexandreRCS <ale.maresias@gmail.com>
2024-03-01 03:38:02 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,13 @@
 
+const path = require("path")
+
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      filename: path.resolve(__dirname, "src", "database", "database.db")
     }
   }
+}
 
-};
