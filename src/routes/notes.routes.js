5094ff46b21e47e0b3076ff26b43d897e166fd93
AlexandreRCS <ale.maresias@gmail.com>
2024-03-02 23:02:48 -0300

    Update notes.routes.js

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,10 @@
+const {Routes} = require("express")
+
+const NotesController = require("../controllers/NotesController")
+
+const notesRoutes = Router()
+
+const notesController = new NotesController()
+notesRoutes.post("/:user_id", notesController.create)
+
+module.exports = notesRoutes
