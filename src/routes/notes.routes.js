9bdeacad2c1c71037bdbd21a8f996bee734f226f
AlexandreRCS <ale.maresias@gmail.com>
2024-03-03 20:48:47 -0300

    Update notes.routes.js

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -7,5 +7,5 @@ const notesRoutes = Router()
 const notesController = new NotesController()
 
 notesRoutes.post("/:user_id", notesController.create)
-
+notesRoutes.get("/:id", notesController.show)
 module.exports = notesRoutes
