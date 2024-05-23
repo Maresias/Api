65d2baf4297d1cd178b29d812b8ef19b7dec03ed
AlexandreRCS <ale.maresias@gmail.com>
2024-05-22 21:59:22 -0300

    Update SessionsController.js

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,13 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
+
 class SessionsController {
     async create(request, response ) {
         const { email, password } = request.body
+        const user = await knex("users").where({email}).first()
         
-        return response.json({ email, password})
+        return response.json(user)
     }
 }
 
