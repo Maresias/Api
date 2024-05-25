bd29187e2094ad85bfb0f04178f4f2c4e5ee4dbd
AlexandreRCS <ale.maresias@gmail.com>
2024-05-24 21:14:27 -0300

    Update ensureAuthenticated.js

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -1,3 +1,14 @@
 const [ verify ] = require("jsonwebtoken")
-const AppErrorm = require("../utils/AppError")
+const AppError = require("../utils/AppError")
 const authConfig = require("../configs/auth")
+
+
+function ensureAuthentid(request, response, next){
+    const authHeader = request.headers.authorization
+
+    if (!authHeader){
+        throw new AppError("JWT Token inválido", 401)
+    }
+
+    const [, token ] = authHeader.split(" ")
+}
