3593490f3b0903fecc1cb9f870f9bbe53e2777cf
AlexandreRCS <ale.maresias@gmail.com>
2024-05-25 23:57:26 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,12 +8,15 @@ const express = require('express')
 
 const routes = require("./routes")
 
+const uploadConfig = require("./configs/upload")
+
 migrationsRun()
 
 
 const app = express()
 app.use(express.json())
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes)
 
