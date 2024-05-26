f05000def217b9174f7527ec9db870880fa68376
AlexandreRCS <ale.maresias@gmail.com>
2024-05-25 21:59:50 -0300

    multer

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,3 +1,5 @@
 const path = require("path")
+const multer = require("multer")
 
 const TPM_FLODER = path.resolve(__dirname,  "..", "..", "tpm")
+const UPLOADS_FOLDER = path.resolve(__dirname, "uploads")
