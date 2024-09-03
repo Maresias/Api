a04ea48902e007ac3ed56888bd3c7b0c6be7d5c1
AlexandreRCS <ale.maresias@gmail.com>
2024-09-02 23:05:49 -0300

    Update userCreateServices.spec.js

--- src/services/userCreateServices.spec.js
+++ src/services/userCreateServices.spec.js
@@ -3,6 +3,13 @@ const UserRepositoriesInMemory = require("../repositories/UserRepositoriesInMemo
 const AppError = require("../utils/AppError")
 
 describe("UserCreateServices", () => {
+    let userRepositoriesInMemory = null
+    let userCreateServices = null
+
+    beforeEach(() =>{
+        userRepositoriesInMemory = new UserRepositoriesInMemory()
+        userCreateServices = new UserCreateServices(userRepositoriesInMemory)
+    })
 
     it("user should be create",  async () => {
 
@@ -12,8 +19,6 @@ describe("UserCreateServices", () => {
             password: "123"
         }
     
-        const userRepositoriesInMemory = new UserRepositoriesInMemory()
-        const userCreateServices = new UserCreateServices(userRepositoriesInMemory)
         const userCreated =  await userCreateServices.execute(user)
     
         expect(userCreated).toHaveProperty("id")
@@ -31,9 +36,6 @@ describe("UserCreateServices", () => {
             email: "test@test.com",
             password: "456"
         }
-
-        const userRepository = new UserRepositoriesInMemory()
-        const userCreateServices = new UserCreateServices(userRepository)
 
         await userCreateServices.execute(user1)
         await expect(userCreateServices.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso"))
