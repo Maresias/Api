cd9a2944ac233911788bc4a8624c9426c859f2fb
AlexandreRCS <ale.maresias@gmail.com>
2024-09-02 21:41:53 -0300

    Update UserRepositoriesInMemory.js

---
+++ src/repositories/UserRepositoriesInMemory.js
@@ -0,0 +1,20 @@
+class UserRepositoriesInMemory {
+    users = []
+
+    async create ({ email, name, password }){
+        const user = {
+            id: Math.floor(Math.random() * 1000) + 1,
+            email,
+            name,
+            password
+        }
+
+        this.users.push(user)
+
+        return user
+    }
+
+    async findByEmail( email ){
+        return this.users.find( user => user.email === email )
+    }
+}
